49f7e0afff1e9eb024a13cf8cd7eae9a1023a505
Artyom Gabtraupov <artyomgv05@gmail.com>
2024-07-19 19:44:58 -0400

    contact form

--- src/components/BottomBar.jsx
+++ src/components/BottomBar.jsx
@@ -13,22 +13,23 @@ const data = [
 
 ]
 
+
 const BottomBar = () => {
-
+    
     const [toggleIcon, setToggleIcon] = useState(false);
-    const [location, setLocation] = useState('GaTalinie');
-
+    const [location, setLocation] = useState(data[0].label);
+    
     const handleToggleIcon = () => {
         setToggleIcon(!toggleIcon);
     }
-
+    
     return (
         <nav className="bottombar">
             <ul className={`bottombar__menu ${toggleIcon ? 'active' : ""} `} >
                 {
                     data.map((item, key) => (
                         <li key={key} className="bottombar__menu__link">
-                            <button className={location === item.label ? 'active' : ''}>
+                            <button className={location === item.label ? 'active' : ''} onClick={() => setLocation(item.label)}>
                                 <span className="bottombar__menu__link__span">
                                     {item.label}
                                 </span>
