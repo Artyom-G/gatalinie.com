82950a096c76e6d8353744f2cc479f7087a8c7cb
Artyom Gabtraupov <artyomgv05@gmail.com>
2024-07-28 12:36:15 -0400

    md image embed changes

---
+++ src/components/ImageEmbed.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './ImageEmbed.scss';
+
+const ImageEmbed = ({ src, alt }) => {
+    let imageSrc = src;
+
+    if (src.startsWith('./') || src.startsWith('/') || src.startsWith('../')) {
+        try {
+            imageSrc = require(`${src}`).default;
+        } catch (err) {
+            console.error(`Error loading image: ${src}`, err);
+        }
+    }
+
+    return (
+        <div className="image-embed">
+            <img src={require(`${src}`)} alt={alt} />
+        </div>
+    );
+};
+
+export default ImageEmbed;
